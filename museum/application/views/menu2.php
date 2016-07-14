42672f1b36dd00ea44598764397b1c8d2a64ae08
ypandoo <ypandoo@126.com>
2016-07-14 22:18:40 +0800

    museum -- modify menu link

--- museum/application/views/menu2.php
+++ museum/application/views/menu2.php
@@ -73,15 +73,14 @@ background-size: cover;
 
     <div class="menu" id="menu" style="display:none">
 
-      <a href='<?php echo base_url('pages/view/about') ?>'><div class="menu_item" style="margin-top:8px"></div></a>
+      <!-- <a href='<?php echo base_url('pages/view/about') ?>'><div class="menu_item" style="margin-top:8px"></div></a>
       <a href='<?php echo base_url('pages/view/new_expo') ?>'><div class="menu_item" ></div></a>
       <a href='<?php echo base_url('pages/view/layout') ?>'><div class="menu_item" ></div></a>
       <a href='<?php echo base_url('pages/view/expo_review') ?>'><div class="menu_item" ></div></a>
       <a href='<?php echo base_url('pages/view/dynamic') ?>'><div class="menu_item" ></div></a>
       <a href='<?php echo base_url('pages/view/instruction') ?>'><div class="menu_item"></div></a>
       <a href='<?php echo base_url('pages/view/construction') ?>'><div class="menu_item" ></div></a>
-      <a href='<?php echo base_url('pages
-      /view/protect') ?>'><div class="menu_item"></div></a>
+      <a href='<?php echo base_url('pages/view/protect') ?>'><div class="menu_item"></div></a> -->
 
     </div>
 </body>
