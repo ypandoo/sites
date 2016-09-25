e7fbaf42b68ef0b0a4c080254b445a49d28661a7
ypandoo <ypandoo@126.com>
2016-09-25 20:06:33 +0800

    musuem -- add 360

--- museum/application/views/360.php
+++ museum/application/views/360.php
@@ -187,11 +187,13 @@
 
 <script>
 
-$(document).ready(function(){
-  $(".panorama").panorama_viewer({
-    repeat: true
-  });
-});
+$(".panorama").panorama_viewer({
+   repeat: true,              // The image will repeat when the user scroll reach the bounding box. The default value is false.
+   direction: "horizontal",    // Let you define the direction of the scroll. Acceptable values are "horizontal" and "vertical". The default value is horizontal
+   animationTime: 1000,         // This allows you to set the easing time when the image is being dragged. Set this to 0 to make it instant. The default value is 700.
+   easing: "ease-out",         // You can define the easing options here. This option accepts CSS easing options. Available options are "ease", "linear", "ease-in", "ease-out", "ease-in-out", and "cubic-bezier(...))". The default value is "ease-out".
+   overlay: false               // Toggle this to false to hide the initial instruction overlay
+ });
 
 </script>
 </html>
