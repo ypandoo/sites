47153df94cc5ec2d5fcae26ec35b2f85cee017e9
ypandoo <ypandoo@126.com>
2016-10-19 07:43:50 +0800

    vnewcd cases/hawaii

--- vnewcd/cases/hawaii/query_words.php
+++ vnewcd/cases/hawaii/query_words.php
@@ -10,12 +10,19 @@ $dbpw = 'bK4cJqcVx5cvcXQ5';
 $dbname='customer';
 $dbobj->connect($dbhost, $dbuser, $dbpw, $dbname);
 
-$sql = "select * from `t_hawaii`";
+$sql = "select * from `t_hawaii` ORDER BY `VOTE` DESC limit ". $start." ,10";
 $result = $dbobj->query($sql);
 
 while($rows[] = $dbobj->fetch_assoc($result));
 {
 	array_pop($rows);
+}
+
+if($rows == '' || is_null($rows) || count($rows) == 0)
+{
+	$json_arr =array('status'=>2);
+	echo json_encode($json_arr);
+	die;
 }
 
 $json_arr =array('status'=>1, 'data'=>$rows);
