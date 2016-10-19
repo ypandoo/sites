485d6cc8fc6475430351b483162732e78b5752aa
ypandoo <ypandoo@126.com>
2016-10-19 21:19:22 +0800

    vnewcd -- update hawaii

--- vnewcd/cases/hawaii/save_words.php
+++ vnewcd/cases/hawaii/save_words.php
@@ -6,15 +6,27 @@ $name   = $_REQUEST['NAME'];
 $age = $_REQUEST['AGE'];
 $des = $_REQUEST['DESCRIPTION'];
 $pic = $_REQUEST['PIC'];
-/*$to = '陈坤';
-$from   = '杨雷';
-$userid = 'userid';
-$nickname= 'nickname';
-$text1 = 'text1';
-$text2 = 'text2';*/
 
-//Save pic
-//save img
+require_once('vnewMysql.php');
+$dbobj = new db_mysql;
+$dbhost = '114.55.112.244:3306';
+$dbuser = 'vnewcd';
+$dbpw = 'bK4cJqcVx5cvcXQ5';
+$dbname='customer';
+$dbobj->connect($dbhost, $dbuser, $dbpw, $dbname);
+
+$sql = "select * from `t_hawaii` where `TEL` = '".$phone."'";
+$result = $dbobj->query($sql);
+// not allow drawing
+if( $dbobj->num_rows($result) > 0)
+{
+	$sql = "UPDATE `t_hawaii` set `NAME` ='".$name."', `AGE` = '".$age."',`DESCRIPTION`='".$des."' where `TEL` = '".$phone."'";
+	$result = $dbobj->query($sql);
+	$json_arr =array('status'=>1, 'msg'=>'修改资料成功!');
+	echo json_encode($json_arr);
+	exit;
+}
+
 $ex = explode(",", $pic);//分割data-url数据
 $filter=explode('/', trim($ex[0],';base64'));//获取文件类型
 $ss = base64_decode(str_replace($filter[1] , '', $ex[1]));//图片解码
@@ -37,26 +49,6 @@ else{
 	$json_arr =array('status'=>1005, 'msg'=>'照片存储失败！请联系开发商！');
 	echo json_encode($json_arr);
 	die;
-}
-
-require_once('vnewMysql.php');
-$dbobj = new db_mysql;
-$dbhost = '114.55.112.244:3306';
-$dbuser = 'vnewcd';
-$dbpw = 'bK4cJqcVx5cvcXQ5';
-$dbname='customer';
-$dbobj->connect($dbhost, $dbuser, $dbpw, $dbname);
-
-$sql = "select * from `t_hawaii` where `TEL` = '".$phone."'";
-$result = $dbobj->query($sql);
-// not allow drawing
-if( $dbobj->num_rows($result) > 0)
-{
-	$sql = "UPDATE `t_hawaii` set `NAME` ='".$name."', `AGE` = '".$age."',`DESCRIPTION`='".$des."', `PIC` = '".$picname."' where `TEL` = '".$phone."'";
-	$result = $dbobj->query($sql);
-	$json_arr =array('status'=>1, 'msg'=>'修改资料成功');
-	echo json_encode($json_arr);
-	exit;
 }
 
 $sql = "INSERT INTO `t_hawaii` (TEL, NAME,AGE, DESCRIPTION, PIC)
