3371ff102391bdb65a7b2eed04aace91e257a4d2
ypandoo <ypandoo@126.com>
2016-12-25 21:43:15 +0800

    museum -- update visual

--- museum/application/views/about.php
+++ museum/application/views/about.php
@@ -34,7 +34,7 @@
   </ol>
 
   <!-- Wrapper for slides -->
-  <div class="carousel-inner">
+  <div class="carousel-inner" role="listbox">
       <div class="item active">
         <img src="<?php echo base_url('application/views/assets/front/img/banner_about1.jpg') ?>" alt="First slide"/>
       </div>
@@ -47,10 +47,10 @@
   </div>
 
   <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left"></span>
   </a>
-  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+  <a class="right carousel-control" href="#carousel-example-generic"  role="button" data-slide="next">
       <span class="glyphicon glyphicon-chevron-right"></span>
   </a>
   </div><!-- /carousel -->
