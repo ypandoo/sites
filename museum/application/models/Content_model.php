fb8b224297e034d4c84215bd2402f8d7b96e952b
ypandoo <ypandoo@126.com>
2017-01-01 10:48:09 +0800

    museum -- update audio

--- museum/application/models/Content_model.php
+++ museum/application/models/Content_model.php
@@ -53,6 +53,7 @@ class Content_model extends CI_Model
     public function getList($list_type, $page_start)
     {
         $interval = $this->config->item('page_interval');
+        $this->db->order_by('PUBLISH_TIME', 'DESC');
         $query = $this->db->get_where('T_CONTENT', array('CONTENT_TYPE' => $list_type), $interval, $page_start);
         $data["success"] = true;
         $data["errorCode"] = 0;
